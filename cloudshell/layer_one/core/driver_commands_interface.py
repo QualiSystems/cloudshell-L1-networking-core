58efa52b5a0abfee2a3fdfb82ea91a0cc8faf8d4
Yaroslav Nikonorov <yar@MacBook-Pro-Yaroslav.local>
2017-09-04 13:27:51 +0300

    Added docstrings

--- cloudshell/layer_one/core/driver_commands_interface.py
+++ cloudshell/layer_one/core/driver_commands_interface.py
@@ -9,44 +9,107 @@ class DriverCommandsInterface(object):
 
     @abstractmethod
     def login(self, address, username, password):
+        """
+        Login driver command, 
+        :param address: 
+        :param username: 
+        :param password: 
+        :return: 
+        """
         pass
 
     @abstractmethod
     def get_resource_description(self, address):
+        """
+        GetResourceDescription driver command
+        :param address: 
+        :return: 
+        """
         pass
 
     @abstractmethod
     def map_bidi(self, src_port, dst_port):
+        """
+        MapBidi driver command
+        :param src_port: 
+        :param dst_port: 
+        :return: 
+        """
         pass
 
     @abstractmethod
     def map_uni(self, src_port, dst_port):
+        """
+        MapUni driver command
+        :param src_port: 
+        :param dst_port: 
+        :return: 
+        """
         pass
 
     @abstractmethod
     def map_clear_to(self, src_port, dst_port):
+        """
+        MapClearTo driver command
+        :param src_port: 
+        :param dst_port: 
+        :return: 
+        """
         pass
 
     @abstractmethod
     def map_clear(self, ports):
+        """
+        MapClear driver command
+        :param ports: 
+        :return: 
+        """
         pass
 
     @abstractmethod
     def get_attribute_value(self, address, attribute_name):
+        """
+        GetAttributeValue driver command
+        :param address: 
+        :param attribute_name: 
+        :return: 
+        """
         pass
 
     @abstractmethod
     def set_attribute_value(self, address, attribute_name, attribute_value):
+        """
+        SetAttributeValue driver command
+        :param address: 
+        :param attribute_name: 
+        :param attribute_value: 
+        :return: 
+        """
         pass
 
     @abstractmethod
     def get_state_id(self):
+        """
+        GetStateId driver command
+        :return: 
+        """
         pass
 
     @abstractmethod
     def set_state_id(self, state_id):
+        """
+        SetStateId driver command
+        :param state_id: 
+        :return: 
+        """
         pass
 
     @abstractmethod
     def map_tap(self, src_port, dst_port):
+        """
+        MapTap driver command
+        :param src_port: 
+        :param dst_port: 
+        :return: 
+        """
         pass
