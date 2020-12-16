98c9026c15e6d1b98f7598c623f6047ecf994ca1
Kyrylo Maksymenko <saklar13@gmail.com>
2020-12-16 15:13:38 +0200

    add pre-commit, tox

--- cloudshell/layer_one/core/helper/xml_logger.py
+++ cloudshell/layer_one/core/helper/xml_logger.py
@@ -3,21 +3,22 @@ import re
 
 
 class XMLLogger(object):
-    """Simple logger used for xml"""
+    """Simple logger used for xml."""
+
     PASSWORD_DISPLAY = "Password>*******</"
 
     def __init__(self, path):
         try:
             os.makedirs(os.path.dirname(path))
-        except:
+        except Exception:
             pass
-        self._descriptor = open(path, 'w+')
+        self._descriptor = open(path, "w+")
 
     def __del__(self):
         self._descriptor.close()
 
     def _write_data(self, data):
-        self._descriptor.write(data + '\r\n')
+        self._descriptor.write(data + "\r\n")
         self._descriptor.flush()
 
     def info(self, data):
